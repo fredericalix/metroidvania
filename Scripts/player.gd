a8175fe94c36192c696e0505c23a132b267b2444
Frédéric Alix <frederic.alix@pancasat.com>
2023-04-10 11:52:40 +0200

    Coding the player movement

---
+++ Scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var speed = 200.0
+
+func _physics_process(delta):
+	Move(delta)
+	move_and_slide()
+	
+func Move(delta): 
+	var movement = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if movement != 0:
+		if movement >  0.0:
+			velocity.x += speed * delta
+			velocity.x = clamp(speed, 100, speed)
+			$Sprite2D.flip_h = false
+			$anim.play("Walk")
+		
+		if movement < 0.0:
+			velocity.x -= speed * delta
+			velocity.x = clamp(speed, -100, -speed)
+			$Sprite2D.flip_h = true
+			$anim.play("Walk")
+	
+	if movement == 0.0:
+		velocity.x = 0.0
+		$anim.play("Idle")
