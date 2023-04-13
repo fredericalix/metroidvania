12e08bcd2a81f8151683db048a785bb32f303378
Frédéric Alix <frederic.alix@pancasat.com>
2023-04-13 07:50:23 +0200

    Calling the right animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,7 +6,7 @@ var jump = 400
 
 func _physics_process(delta):
 	Move(delta)
-	Jump()
+
 	velocity.y += gravity
 	move_and_slide()
 	
@@ -28,7 +28,17 @@ func Move(delta):
 	if movement == 0.0:
 		velocity.x = 0.0
 		$anim.play("Idle")
+		
+	if is_on_floor() && Input.is_action_just_pressed("ui_jump"):
+		Jump()
+		
+	if !is_on_floor():
+		$anim.play("Jump")
+		
+	if !is_on_floor() && velocity.y > 10:
+		$anim.play("Fall")
 
 func Jump():
-	if Input.is_action_just_pressed("ui_jump"):
-		velocity.y -= jump
+	$anim.play("Jump")
+	velocity.y -= jump
+	
